ccf141e6bfe0b8eec10ebb94096b6df21d2904e1
agusrapisarda2 <agustinarapisarda@gmail.com>
2023-07-26 17:36:35 -0300

    segundo-commit
    segundo-commit

---
+++ main.js
@@ -0,0 +1,42 @@
+console.log('A')
+var cantidadDeGatos = 3; 
+var emojisDeGatos = ["😺", "😸", "😹"]; 
+for (var i = 1; i <= cantidadDeGatos; i++) { 
+  var emojis = emojisDeGatos[(i - 1) % 3]; 
+  console.log("A: Gato #" + i + ": " + emojis); 
+}
+
+
+       
+       
+       
+
+
+
+
+
+
+       
+
+       
+
+
+
+     
+     
+    
+     
+       
+
+
+        
+        
+        
+       
+       
+       
+       
+       
+
+
+       
