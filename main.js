c202d3d18248079d8d604b6b30c7dfa3f6b1d29b
agusrapisarda2 <agustinarapisarda@gmail.com>
2023-07-28 11:09:25 -0300

    Ejercicio 1

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 console.log('A')
-var cantidadDeGatos = 3; 
-var emojisDeGatos = ["😺", "😸", "😹"]; 
+var cantidadDeGatos = 3
+var emojisDeGatos = ["😺", "😸", "😹"]
 for (var i = 1; i <= cantidadDeGatos; i++) { 
-  var emojis = emojisDeGatos[(i - 1) % 3]; 
-  console.log("A: Gato #" + i + ": " + emojis); 
+  var emojis = emojisDeGatos[(i - 1) % 3]
+  console.log("A: Gato #" + i + ": " + emojis)
 }
 
 
