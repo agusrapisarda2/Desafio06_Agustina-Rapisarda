0670b55ad6c5a3f1abf12e3da054262e2149433c
agusrapisarda2 <agustinarapisarda@gmail.com>
2023-07-28 11:02:55 -0300

    Segundo ejercicio terminado

--- main2.js
+++ main2.js
@@ -1,3 +1,13 @@
-console.log('B')
-     var cantidadDeGatos = 5 
+    console.log('B')
+    
+    var cantidadDeGatos = 5
      var cantidadDePasos = 3 
+     var emojiGato = ['🐈']
+     var emojiPasos = [ '🐾']
+     for (var i = 1; i <= cantidadDeGatos; i++) {
+        var pasos = '';
+        for (var j = 1; j <= cantidadDePasos; j++) {
+          pasos = pasos + emojiPasos;
+        }
+    console.log('Gato#' + i + ':' + emojiGato  + pasos )
+     }
